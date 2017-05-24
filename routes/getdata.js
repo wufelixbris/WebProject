5456651c3d54cb5710b0b126a88678f2f493f5a5
wufelixbris <fw16243@my.bristol.ac.uk>
2017-05-24 22:23:05 +0800

    getdata successfully slight changes to the header template

--- routes/getdata.js
+++ routes/getdata.js
@@ -7,20 +7,19 @@ var router = express.Router();
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('testDB.db');
 
-/* GET registration page. */
+/* GET data page. */
 
 router.get('/',function(req, res, next){
+    var resultArray = [];
     db.serialize(function(){
         db.each("SELECT * FROM Person", function(err, row){
-            res.render('register', {
-                title: 'register',
-                email: row.email,
-                username: row.username,
-                password: row.password
-            });
+            resultArray.push(row);
         });
     });
-    db.close();
+    db.all("SELECT * FROM Person", function(err, rows){
+        console.log(rows);
+        res.render('getdata', {title: 'getdata', items: rows});
+    });
 });
 
 module.exports = router;
