de0fd23685ceaf90f19392fa489027e17fde5713
wufelixbris <fw16243@my.bristol.ac.uk>
2017-05-25 22:43:56 +0800

    listing on home page

--- routes/home.js
+++ routes/home.js
@@ -1,4 +1,11 @@
 'use strict';
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('testDB.db');
 exports.index = function(req, res) {
-  res.render('home', { title: 'Brizzar' });
+  db.serialize(function(){
+    db.all('SELECT * FROM Item', function(err, row){
+      res.render('home', {title: 'Brizzar', items: row});
+    });
+  });
+  //res.render('home', { title: 'Brizzar' });
 };
